414d060f674f052dd4c6e0c13c91fc1c3f41514f
18142014@edu.cc.saga-u.ac.jp <18142014@edu.cc.saga-u.ac.jp>
2020-10-23 14:28:00 +0900

    bootstrapを使い18142014大浦菜都記が変更した。

---
+++ bootstrap/js/jquery-natsuki.js
@@ -0,0 +1,11 @@
+$('#btnbutton').click(function(){
+    $('#btnbutton').html('クリックされました');
+
+});
+$('#btnbutton').css("color","blue");
+
+$('button').css("background-color","pink");
+
+$('#btnbutton').click(function(){
+    window.location.href = "/index.html";
+});
